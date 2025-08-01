0757830c5f8d5d51842ab4dfadf472b01f77eb94
CerfSoleil <robedj12@gmail.com>
2025-08-01 11:00:50 -0500

    Notes for NavBar

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+//This should have the following categories:
+
+// Character Creation
+/// Classes
+/// Races (This needs a toggle for the planet players are on.)
+/// Origins
+
+// Lore
+/// Planets
+/// Planes
+//// Continent
+///// Country
+
+// Gods
+/// All Gods
+/// Gods by Pillar
+
+// Maps
+
+// Players
