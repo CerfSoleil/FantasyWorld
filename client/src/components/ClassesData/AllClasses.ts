da097a6ed8d45db2ee49870edffb38d9344e1a86
CerfSoleil <robedj12@gmail.com>
2025-08-02 15:47:04 -0500

    Added Fighter data

--- client/src/components/ClassesData/AllClasses.ts
+++ client/src/components/ClassesData/AllClasses.ts
@@ -2,11 +2,13 @@
 
 import { ClassBase } from "./ClassBase";
 import { bardClass, bardLevels } from "./Bard";
+import { fighterClass, fighterLevels } from "./Fighter";
 import { rogueClass, rogueLevels } from "./Rogue";
 
-export const allClassCards: ClassBase[] = [bardClass, rogueClass];
+export const allClassCards: ClassBase[] = [bardClass, fighterClass, rogueClass];
 
 export const classLevelMap: Record<string, { level: number; [key: string]: any }[]> = {
   bard: bardLevels,
+  fighter: fighterLevels,
   rogue: rogueLevels,
 };
